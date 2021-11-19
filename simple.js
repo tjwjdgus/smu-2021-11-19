7f25b5726995e5266abf19db396e1b7481a7c33a
tjwjdgus <55967207+tjwjdgus@users.noreply.github.com>
2021-11-19 10:02:21 +0900

    최소한의 코드 작성

---
+++ simple.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var app = http.createServer(function(request,response){
+    console.log('request');
+    response.end(`
+    <h1><a href="index.html">WEB</a></h1>
+    <ul>
+        <li><a href="html.html">html</a></li>
+        <li><a href="nodejs.html">nodejs</a></li>
+        <li><a href="mysql.html">mysql</a></li>
+    </ul>
+    <h2>Welcome</h2>
+    Hello WEB!!!!!!!    
+    `);
+});
+app.listen(8080);
